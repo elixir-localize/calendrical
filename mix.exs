019d5307ef03f46f316d309fc596e96387eed31e
Kip Cole <kipcole9@gmail.com>
2026-05-23 23:49:35 +1000

    Require astro ~> 2.2 for the proleptic-Gregorian equinox fix

--- mix.exs
+++ mix.exs
@@ -126,7 +126,7 @@ defmodule Calendrical.MixProject do
   defp deps do
     [
       {:localize, "~> 0.37"},
-      {:astro, "~> 2.0"},
+      {:astro, "~> 2.2"},
       {:tz_world, "~> 1.0", optional: true},
       {:tzdata, "~> 1.1", optional: true},
       {:gettext, "~> 1.0"},
