0a960c84b3677c8e393acc9954067475c9b119dd
Kip Cole <kipcole9@gmail.com>
2026-09-26 12:37:26 +1000

    main uses github deps for localize

--- mix.exs
+++ mix.exs
@@ -174,7 +174,7 @@ defmodule Calendrical.MixProject do
 
   defp deps do
     [
-      {:localize, "~> 1.4"},
+      {:localize, github: "elixir-localize/calendrical", branch: "main"},
       {:astro, "~> 2.7"},
       {:tz_world, "~> 2.3", optional: true},
       {:tz, "~> 0.26", optional: true},
